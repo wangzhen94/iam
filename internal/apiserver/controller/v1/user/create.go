e665977eac60f3e08e34c57f3431e99dd5a92d76
wangzhen <zhen71.wang@getech.cn>
2023-11-16 00:48:04 +0800

    log

--- internal/apiserver/controller/v1/user/create.go
+++ internal/apiserver/controller/v1/user/create.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	v1 "github.com/marmotedu/api/apiserver/v1"
 	"github.com/marmotedu/component-base/pkg/auth"
@@ -23,9 +24,18 @@ func (u *UserController) Create(c *gin.Context) {
 		return
 	}
 
+	if errs := r.Validate(); len(errs) != 0 {
+		core.WriteResponse(c, errors.WithCode(code.ErrValidation, errs.ToAggregate().Error()), nil)
+
+		return
+	}
+
 	r.Password, _ = auth.Encrypt(r.Password)
 	r.Status = 1
 	r.LoginedAt = time.Now()
+
+	s, _ := json.Marshal(&r)
+	log.Infof("create user: %s", s)
 
 	if err := u.srv.Users().Create(c, &r, metav1.CreateOptions{}); err != nil {
 		core.WriteResponse(c, err, nil)
