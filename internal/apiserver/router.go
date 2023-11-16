282866a8b0b1610eaa52723cd7d685d05b98350a
wangzhen <zhen71.wang@getech.cn>
2023-11-16 14:57:03 +0800

    user route

--- internal/apiserver/router.go
+++ internal/apiserver/router.go
@@ -31,6 +31,8 @@ func installController(g *gin.Engine) *gin.Engine {
 		userV1.POST("", userController.Create)
 		userV1.DELETE("/:name", userController.Delete)
 		userV1.GET("", userController.List)
+		userV1.PUT("", userController.Update)
+		userV1.GET("/:name", userController.Get)
 
 	}
 
