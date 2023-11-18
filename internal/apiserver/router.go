0d5e44746bd06803d584f3599caa8263db6012e3
wangzhen <zhen71.wang@getech.cn>
2023-11-18 21:42:41 +0800

    test

--- internal/apiserver/router.go
+++ internal/apiserver/router.go
@@ -1,9 +1,14 @@
+// Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package apiserver
 
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/marmotedu/component-base/pkg/core"
 	"github.com/marmotedu/errors"
+
 	"github.com/wangzhen94/iam/internal/apiserver/controller/v1/policy"
 	"github.com/wangzhen94/iam/internal/apiserver/controller/v1/secret"
 	"github.com/wangzhen94/iam/internal/apiserver/controller/v1/user"
@@ -23,9 +28,10 @@ func installMiddleware(g *gin.Engine) {
 
 func installController(g *gin.Engine) *gin.Engine {
 	// Middlewares.
-	jwtStrategy := newJWTAuth().(auth.JWTStrategy)
+	jwtStrategy, _ := newJWTAuth().(auth.JWTStrategy)
 	g.POST("/login", jwtStrategy.LoginHandler)
 	g.POST("/logout", jwtStrategy.LogoutHandler)
+	// Refresh time can be longer than token timeout
 	g.POST("/refresh", jwtStrategy.RefreshHandler)
 
 	auto := newAutoAuth()
@@ -33,57 +39,51 @@ func installController(g *gin.Engine) *gin.Engine {
 		core.WriteResponse(c, errors.WithCode(code.ErrPageNotFound, "Page not found."), nil)
 	})
 
+	// v1 handlers, requiring authentication
 	storeIns, _ := mysql.GetMySQLFactoryOr(nil)
 	v1 := g.Group("/v1")
 	{
-		userV1 := v1.Group("/user")
+		// user RESTful resource
+		userv1 := v1.Group("/user")
 		{
 			userController := user.NewUserController(storeIns)
-			userV1.POST("", userController.Create)
-			userV1.Use(auto.AuthFunc(), middleware.Validation())
-			userV1.DELETE("/:name", userController.Delete)
-			userV1.PUT(":name/change-password", userController.ChangePassword)
-			userV1.GET("", userController.List)
-			userV1.PUT("", userController.Update)
-			userV1.GET("/:name", userController.Get)
+
+			userv1.POST("", userController.Create)
+			userv1.Use(auto.AuthFunc(), middleware.Validation())
+			// v1.PUT("/find_password", userController.FindPassword)
+			userv1.DELETE(":name", userController.Delete) // admin api
+			userv1.PUT(":name/change-password", userController.ChangePassword)
+			userv1.PUT(":name", userController.Update)
+			userv1.GET("", userController.List)
+			userv1.GET(":name", userController.Get) // admin api
 		}
 
 		v1.Use(auto.AuthFunc())
-		secretV1 := v1.Group("/secret")
-		{
-			secretController := secret.NewSecretController(storeIns)
-			secretV1.GET("/:name", secretController.Get)
-			secretV1.POST("", secretController.Create)
-			secretV1.PUT("/:name", secretController.Get)
-			secretV1.GET("", secretController.List)
-			secretV1.DELETE("", secretController.Delete)
-		}
 
-		policyV1 := v1.Group("/policy")
+		// policy RESTful resource
+		policyv1 := v1.Group("/policy")
 		{
 			policyController := policy.NewPolicyController(storeIns)
-			policyV1.POST("", policyController.Create)
-			policyV1.DELETE("/:name", policyController.Delete)
-			policyV1.PUT("/:name", policyController.Update)
-			policyV1.GET("", policyController.List)
-			policyV1.GET("/:name", policyController.Get)
+
+			policyv1.POST("", policyController.Create)
+			policyv1.DELETE(":name", policyController.Delete)
+			policyv1.PUT(":name", policyController.Update)
+			policyv1.GET("", policyController.List)
+			policyv1.GET(":name", policyController.Get)
+		}
+
+		// secret RESTful resource
+		secretv1 := v1.Group("/secret")
+		{
+			secretController := secret.NewSecretController(storeIns)
+
+			secretv1.POST("", secretController.Create)
+			secretv1.DELETE(":name", secretController.Delete)
+			secretv1.PUT(":name", secretController.Update)
+			secretv1.GET("", secretController.List)
+			secretv1.GET(":name", secretController.Get)
 		}
 	}
 
 	return g
-}
-
-func getUser(name string) error {
-	if err := queryDataBase(name); err != nil {
-		return errors.Wrap(err, "get user error")
-	}
-	return nil
-}
-
-func queryDataBase(name string) error {
-	if "wang" == name {
-		return nil
-	} else {
-		return errors.WithCode(code.ErrDatabase, "user '%s' not found", name)
-	}
 }
