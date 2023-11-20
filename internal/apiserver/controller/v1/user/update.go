bd5dbfd691a9a2d55f7eea7a283de270c2d8c975
wangzhen <zhen71.wang@getech.cn>
2023-11-21 00:25:07 +0800

    update

--- internal/apiserver/controller/v1/user/update.go
+++ internal/apiserver/controller/v1/user/update.go
@@ -3,7 +3,6 @@ package user
 import (
 	"github.com/gin-gonic/gin"
 	v1 "github.com/marmotedu/api/apiserver/v1"
-	"github.com/marmotedu/component-base/pkg/auth"
 	"github.com/marmotedu/component-base/pkg/core"
 	metav1 "github.com/marmotedu/component-base/pkg/meta/v1"
 	"github.com/marmotedu/errors"
@@ -12,44 +11,35 @@ import (
 )
 
 func (u *UserController) Update(c *gin.Context) {
-	var r *v1.User
-	oldPassword := c.Query("password")
+	log.L(c).Info("update user function called.")
+
+	var r v1.User
+
 	if err := c.ShouldBindJSON(&r); err != nil {
 		core.WriteResponse(c, errors.WithCode(code.ErrBind, err.Error()), nil)
 
 		return
 	}
 
-	log.L(c).Infof("update user %s, function called.", r.Name)
-
-	user, err := u.srv.Users().Get(c, r.Name, metav1.GetOptions{})
+	user, err := u.srv.Users().Get(c, c.Param("name"), metav1.GetOptions{})
 	if err != nil {
 		core.WriteResponse(c, err, nil)
 
 		return
 	}
 
-	err = checkOldPassword(user, oldPassword)
-	if err != nil {
-		core.WriteResponse(c, errors.WithCode(code.ErrPasswordIncorrect, err.Error()), nil)
-
-		return
-	}
 	user.Nickname = r.Nickname
 	user.Email = r.Email
 	user.Phone = r.Phone
 	user.Extend = r.Extend
-	user.Password = r.Password
 
-	if errs := user.Validate(); len(errs) != 0 {
-		log.Errorf("validate failed %s .", errs.ToAggregate().Error())
+	if errs := user.ValidateUpdate(); len(errs) != 0 {
 		core.WriteResponse(c, errors.WithCode(code.ErrValidation, errs.ToAggregate().Error()), nil)
 
 		return
 	}
 
-	user.Password, _ = auth.Encrypt(user.Password)
-
+	// Save changed fields.
 	if err := u.srv.Users().Update(c, user, metav1.UpdateOptions{}); err != nil {
 		core.WriteResponse(c, err, nil)
 
@@ -57,17 +47,4 @@ func (u *UserController) Update(c *gin.Context) {
 	}
 
 	core.WriteResponse(c, nil, user)
-}
-
-func checkOldPassword(user *v1.User, oldPassword string) error {
-	if oldPassword == "" {
-		return nil
-	}
-
-	log.Infof("old password %s, db password %s", oldPassword, user.Password)
-	if err := user.Compare(oldPassword); err != nil {
-		return errors.WithCode(code.ErrPasswordIncorrect, err.Error())
-	}
-
-	return nil
 }
